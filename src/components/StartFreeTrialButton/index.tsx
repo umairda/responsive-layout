b7efee718cf17db739d0a9e87b29cd686b854811
Umair Ansari <umairda@gmail.com>
2022-09-01 16:22:44 -0700

    Create StartFreeTrialButton component

---
+++ src/components/StartFreeTrialButton/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './StartFreeTrialButton.module.css'
+
+function StartFreeTrialButton() {
+    return (
+        <a className={styles.button}  href="/" alt="Start Free Trial">Start Free Trial</a>
+    )
+}
+
+export default StartFreeTrialButton
