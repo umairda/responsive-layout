9567f7986080d21e9af166f4c506f97bb0556dd0
Umair Ansari <umairda@gmail.com>
2022-09-01 15:59:21 -0700

    Create HamburgerMenu component

---
+++ src/components/Mobile/HamburgerMenu/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent as HamburgerMenuIcon }  from '../../../assets/HamburgerMenu.svg'
+
+function HamburgerMenu() {
+    return <div>
+        <HamburgerMenuIcon />
+    </div>
+}
+
+export default HamburgerMenu
