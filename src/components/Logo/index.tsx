a0293127901a9f1240047fb646a779dfe168348a
Umair Ansari <umairda@gmail.com>
2022-09-01 16:03:31 -0700

    Create Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent as LogoIcon } from '../../assets/Logo.svg'
+
+function Logo() {
+    return (
+        <div>
+            <LogoIcon />
+        </div>
+    )
+}
