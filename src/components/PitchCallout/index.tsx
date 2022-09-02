f1c9755419d99170dff6c01630cab0c5a84470bc
Umair Ansari <umairda@gmail.com>
2022-09-02 10:35:54 -0700

    Create PitchCallout component

---
+++ src/components/PitchCallout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeadingText from '../HeadingText'
+
+function PitchCallout() {
+    return (
+        <>
+            <div>
+                <HeadingText text="A better way to" />
+            </div>
+            <div>
+                <HeadingText text="ship web apps" color="#6366F1" />
+            </div>
+        </>
+    )
+}
+
+export default PitchCallout
