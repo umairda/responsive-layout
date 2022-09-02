bb32e87ce71b34c27aff25774aeb328cc06e8aa7
Umair Ansari <umairda@gmail.com>
2022-09-02 11:06:18 -0700

    Create Details text component

---
+++ src/components/DetailsText/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react'
+import styles from 'DetailsText.module.css'
+
+function DetailsText({ children }: { children: ReactNode }) {
+    return (
+        <p className={styles.detailsText}>
+            {children}
+        </p>
+    )
+}
+
+export default DetailsText
