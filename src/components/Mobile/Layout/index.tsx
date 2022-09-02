70ea5b6deff72b58e92b8c4e4da952a722e2ef3b
Umair Ansari <umairda@gmail.com>
2022-09-02 11:25:00 -0700

    Fix mobile nav bar layout

--- src/components/Mobile/Layout/index.tsx
+++ src/components/Mobile/Layout/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import NavBar from '../NavBar'
+import styles from './Layout.module.css'
 
 function Layout() {
     return (
-        <div>
+        <div className={styles.mobileLayoutContainer}>
             <NavBar />
         </div>
     )
--- src/components/Mobile/NavBar/index.tsx
+++ src/components/Mobile/NavBar/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Logo from '../../Logo'
+import { ReactComponent as LogoIcon } from '../../../assets/Logo.svg'
 import HamburgerMenu from '../HamburgerMenu'
 import styles from './NavBar.module.css'
 
 function NavBar() {
     return (
         <div className={styles.navBarContainer}>
-            <Logo />
+            <LogoIcon height={30} width={33} />
             <HamburgerMenu />
         </div>
     )
