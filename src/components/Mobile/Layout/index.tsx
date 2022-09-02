c6021462f3398a3081c354a6e5e4b400d948886d
Umair Ansari <umairda@gmail.com>
2022-09-02 11:39:14 -0700

    Adding remaining components to mobile layout, fix layout/styling

--- src/components/Mobile/Layout/index.tsx
+++ src/components/Mobile/Layout/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import BadgeCallout from '../../BadgeCallout'
+import DescriptionCallout from '../../DescriptionCallout'
+import DetailsCallout from '../../DetailsCallout'
+import Input from '../../Input'
+import PitchCallout from '../../PitchCallout'
+import StartFreeTrialButton from '../../StartFreeTrialButton'
 import NavBar from '../NavBar'
 import styles from './Layout.module.css'
 
@@ -6,6 +12,22 @@ function Layout() {
     return (
         <div className={styles.mobileLayoutContainer}>
             <NavBar />
+            <div className={styles.badgeCalloutContainer}>
+                <BadgeCallout />
+            </div>
+            <div className={styles.pitchCalloutContainer}>
+                <PitchCallout />
+            </div>
+            <div className={styles.descriptionCalloutContainer}>
+                <DescriptionCallout />
+            </div>
+            <div className={styles.emailSignupContainer}>
+                <Input placeholder="Enter your email" />
+                <StartFreeTrialButton color="#6366F1" />
+            </div>
+            <div className={styles.detailsCalloutContainer}>
+                <DetailsCallout />
+            </div>
         </div>
     )
 }
