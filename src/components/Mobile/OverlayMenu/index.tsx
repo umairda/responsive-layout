78d844b02abbcc39987b5fa8504556d39cabb428
Umair Ansari <umairda@gmail.com>
2022-09-02 12:28:42 -0700

    Add overlay menu content/styling

--- src/components/Mobile/OverlayMenu/index.tsx
+++ src/components/Mobile/OverlayMenu/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ParagraphText from '../../ParagraphText';
+import StartFreeTrialButton from '../../StartFreeTrialButton';
 import { useOverlayContext } from './Context';
 import OverlayNavBar from './NavBar';
 import styles from './Overlay.module.css'
@@ -7,6 +9,20 @@ function Overlay() {
     return (
         <div className={styles.overlayContainer}>
             <OverlayNavBar />
+            <div className={styles.linksContainer}>
+                <a href="/" title="Product">Product</a>
+                <a href="/" title="Features">Features</a>
+                <a href="/" title="Marketplace">Marketplace</a>
+                <a href="/" title="Company">Company</a>
+            </div>
+            <div className={styles.freeTrialContainer}>
+                <StartFreeTrialButton color="#4F46E5" />
+            </div>
+            <div className={styles.loginContainer}>
+                <span>Existing customer?</span>
+                {" "}
+                <a href="/" title="login">Login</a>
+            </div>
         </div>
     )
 }
